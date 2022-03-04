1251fff764eb04a91d469be4ff00ac952e1097fb
leo3friedman <73138942+leo3friedman@users.noreply.github.com>
2022-03-04 11:21:28 -0800

    initial prototype calculator from snippet

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log("ran");
+window.onload = () => {
+    const newDiv = document.createElement("div");
+    const input = document.querySelector("[name=price]");
+    const newContent = document.createTextNode(`BREAKEVEN: ${breakEvenPrice(2, .125)}`);
+    const url = window.location.href;
+    const nft = url.replace("https://opensea.io/assets/","").replace("/sell","");
+
+    newDiv.appendChild(newContent);
+
+    input.parentElement.appendChild(newDiv);
+
+    function calculateProfit(price, cost, fees){
+        return price-price * fees - cost
+    }
+    function breakEvenPrice(cost, fees){
+        return (cost /(1-fees)).toFixed(3)
+    }
+
+    newDiv.style.zIndex = "9999";
+
+    input.oninput = () => {
+        if(input.value === ''){
+            newDiv.innerText = `BREAKEVEN: ${breakEvenPrice(2, .125)}`
+        } else {
+            newDiv.innerText = `PROFIT: ${calculateProfit(input.value, 2,.125)}`;
+        }
+    }
+    console.log(nft)
+}
